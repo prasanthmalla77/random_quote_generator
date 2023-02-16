7574fcfc04fd7a1fe228e8d13d514bcc6371cf3e
MALLA NAGA VENKATA PRASANTH NAIDU <110344132+prasanthmalla77@users.noreply.github.com>
2023-02-16 12:06:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const quotes = {
-    "-George Clooney (July 2006)": '“My father gave me the best advice of my life. He said, ‘Whatever you do, don’t wake up at 65 years old and think about what you should have done with your life.'” ',
-    "- J.M. Barrie  (August 1933)": '“Not in doing what you like, but in liking what you do is the secret of happiness.”',
-    "-Katherine Mansfield (April 1941)": ' “Make it a rule of life never to regret and never to look back. Regret is an appalling waste of energy; you can’t build on it; it’s only good for wallowing.”',
-    "-Simone Biles (July/August 2020)": '“I’d rather regret the risks that didn’t work out than the chances I didn’t take at all.”',
-    "-Jonathan Winters (June 1990)": '“If your ship doesn’t come in, swim out to it! "',
-    "-Robert Mansell (August 1933)": '“A pessimist is one who makes difficulties out of his opportunities; an optimist is one who makes opportunities out of his difficulties.” ',
-    "- Archbishop Desmond Tutu (January 2008)": '“My father used to say, ‘Don’t raise your voice. Improve your argument.',
-    "-H.G. Wells  (June 1940)": ' “What on earth would a man do with himself if something did not stand in his way?”',
-    "- Columbia Record (May 1925)": '"I attribute my success to this: I never gave or took any excuse."',
+    "- Jim Rohn": '"Either you run the day, or the day runs you."',
+    "- Henry Ford": '"Whether you think you can or you think you can’t, you’re right."',
+    "- Frank Sinatra": '"The best revenge is massive success."',
+    "- Zig Ziglar": '"People often say that motivation doesn’t last. Well, neither does bathing.  That’s why we recommend it daily."',
+    "- Aristotle": '"There is only one way to avoid criticism: do nothing, say nothing, and be nothing"',
+    "- Jesus": '"Ask and it will be given to you; search, and you will find; knock and the door will be opened for you."',
+    "- Ralph Waldo Emerson": '"The only person you are destined to become is the person you decide to be."',
+    "- Henry David Thoreau": '"Go confidently in the direction of your dreams.  Live the life you have imagined."',
+    "- Erma Bombeck": '"When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me."',
+    "- Booker T. Washington": '"Few things can help an individual more than to place responsibility on him, and to let him know that you trust him."'
+  };
    
   };
 
